338ca7dccf38c548043bf9cb6b583c945e92c3c0
Bridgette Lemus <bridgettelemus@Bridgettes-MacBook-Pro.local>
2022-07-16 11:06:44 -0400

    saturday practice

---
+++ july16.js
@@ -0,0 +1,15 @@
+// let pop = 256
+// let n = 2
+
+
+function whenBuilt (pop, n){
+    let newPeople = n*360
+    let findPop = pop + newPeople
+    return findPop
+}
+
+console.log(whenBuilt(3248,6))
+
+
+
+
