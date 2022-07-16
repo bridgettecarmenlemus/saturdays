dd5068a4faaa72d82de6721baca386ebf474870d
Bridgette Lemus <bridgettelemus@Bridgettes-MacBook-Pro.local>
2022-07-16 12:16:38 -0400

    lab1, "fizzbuzz"

--- lab1.js
+++ lab1.js
@@ -1,3 +1,17 @@
-The FizzBuzz problem is a classic test given in coding interviews. 
-The task is simple: Print integers 1 to N, but print "Fizz" if an integer is divisible by 3,
- "Buzz" if an integer is divisible by 5, and "FizzBuzz" if an integer is divisible by both 3 and 5.
+// The FizzBuzz problem is a classic test given in coding interviews. 
+// The task is simple: Print integers 1 to N, but print "Fizz" if an integer is divisible by 3,
+//  "Buzz" if an integer is divisible by 5, and "FizzBuzz" if an integer is divisible by both 3 and 5.
+
+function FizzBuzz = (n) {
+    for (let i=1;. i<=n; i++){
+    if (n%3===0 && n%5===0){
+        print "FizzBuzz"
+    } else if (n%3 === 0) {
+        print 'fizz'
+    } else if (n%5 === 0) {
+        print 'buzz'
+    }
+    else print n
+}
+}
+FizzBuzz ()
